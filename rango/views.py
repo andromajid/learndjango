5d5e84a66dc1e5583fbc6109ac1ee1b007aa4e49
andro_majid <andro.majid@redawning.com>
2014-07-08 15:51:07 +0700

    test

--- rango/views.py
+++ rango/views.py
@@ -2,11 +2,30 @@
 from django.http import HttpResponse
 from django.template import RequestContext
 from django.shortcuts import render_to_response
+from rango.models import Category, Page
 
 def index(request) :
     context = RequestContext(request)
-    context_dict = {'boldmessage' : 'I\'m Bold font from the context'}
+
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict = {'category_list' : category_list}
     return render_to_response('rango/index.html', context_dict, context)
 
 def about(request) :
     return HttpResponse("Rango Says : here's its the about page!<br /><a href=\"/rango\">home</a>")
+
+def category(request, category_name_url):
+    context = RequestContext(request)
+
+    category_name_url = category_name_url.replace('_', ' ')
+    context_dict = {'category_name' : category_name_url}
+
+    try:
+        category = Category.objects.get(name = category_name_url)
+        pages = Page.object.filter(category = category)
+        context_dict['pages'] = pages
+        context_dict['category'] = category
+    except Category.DoesNotExist :
+        pass
+
+    return render_to_response('rango/category.html', context_dict, context)
